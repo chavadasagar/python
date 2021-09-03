97bd45b2432ddabaefe69360fd134303ed80d27b
sagar chavada <66827189+chavadasagar@users.noreply.github.com>
2021-09-03 04:53:27 -0700

    Update prime_no.py

--- prime_no.py
+++ prime_no.py
@@ -1,10 +1,10 @@
-n = int(input("prime number :"))
-
-m = int(n/2);
+n = int(input("enter no"))
 temp = 0
-x=2
-for x in range(m):
-    if(x%n == 0):
+m = int(n/2)
+
+
+for x in range(2,m):
+    if(n%2 == 0):
         print("not prime no")
         temp = 1
         break
